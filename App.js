c1835021aae989adf4fb045e198b49e9c946b906
OlexandrVaidych <10lexandr.vaidych@gmail.com>
2021-02-22 12:53:47 +0200

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>{`Вайдич Олександр\nГрупа ІО-82\nЗК ІО-8204`}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    textAlign: "center",
+  }
 });
